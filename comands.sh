691ba18ff44ff5487a623a0ac4545a577a5c2a19
Gerd Grimmen <gerd.grimmen.gg@gmail.com>
2025-11-27 23:35:53 +0100

    add caommand.sh

---
+++ comands.sh
@@ -0,0 +1,9 @@
+# You need to install the compiler / cli tools
+elm
+# initializing of a project
+# further info on "elm init": https://github.com/elm/compiler/blob/master/hints/init.md
+elm init
+
+# start a dashboard to choose the example files you want to run 
+elm reactor
+
